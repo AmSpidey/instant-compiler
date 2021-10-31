5adf871a5bf726b13f01515a013d7deea742c2d1
Marta Nowakowska <me.nowakowska@student.uw.edu.pl>
2021-10-31 15:10:34 +0100

    Delete some files.

--- to_pack/llvm_runner.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-./insc_llvm $1.ins
-llvm-as -o $1.bc $1.ll
-llvm-link -o out.bc $1.bc
-lli out.bc
--- to_pack/small_runner.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-llvm-as -o $1.bc $1.ll
-llvm-link -o out.bc $1.bc
-lli out.bc
